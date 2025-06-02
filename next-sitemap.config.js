751c13b1391fb344fac1ba7d8d0267f07ef007e4
Xebarter <sebenock027@gmail.com>
2025-06-02 12:23:54 +0300

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://reverencetechnology.com', // Replace with your actual domain
+  generateRobotsTxt: true, // Optional: generates robots.txt
+};
