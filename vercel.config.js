68d05e6d86f6f81a8b2559afab486aa019a26199
Xebarter <sebenock027@gmail.com>
2025-11-07 22:29:26 +0300

    well

--- vercel.config.js
+++
@@ -1,7 +0,0 @@
-// Vercel configuration for SPA routing
-module.exports = {
-  rewrites: [
-    // Rewrite everything to index.html to support client-side routing
-    { source: '/(.*)', destination: '/index.html' }
-  ]
-};
